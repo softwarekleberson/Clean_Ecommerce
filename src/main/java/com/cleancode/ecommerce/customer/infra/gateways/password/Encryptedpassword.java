7573d482dfaf50e2c2db4e5dbbd9d626e7c7ea65
softwarekleberson <santossilvakleberson@gmail.com>
2025-06-19 12:20:05 -0300

    add passwordEncoder interface and implements passwordEncoder in ifra

---
+++ src/main/java/com/cleancode/ecommerce/customer/infra/gateways/password/Encryptedpassword.java
@@ -0,0 +1,24 @@
+package com.cleancode.ecommerce.customer.infra.gateways.password;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Encryptedpassword implements PasswordEncoder {
+
+	private final PasswordEncoder passwordEncoder;
+
+	public Encryptedpassword(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
+	
+	@Override
+	public String encode(CharSequence rawPassword) {
+		return passwordEncoder.encode(rawPassword);
+	}
+
+	@Override
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		return passwordEncoder.matches(rawPassword, encodedPassword);
+	}
+}
