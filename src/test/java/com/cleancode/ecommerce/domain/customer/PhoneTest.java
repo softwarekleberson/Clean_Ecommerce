879f82358687620d6aae3f6a3432a791a70279d7
softwarekleberson <santossilvakleberson@gmail.com>
2025-07-02 16:08:14 -0300

    test: update PhoneTest with new TypePhone

--- src/test/java/com/cleancode/ecommerce/domain/customer/PhoneTest.java
+++ src/test/java/com/cleancode/ecommerce/domain/customer/PhoneTest.java
@@ -14,24 +14,24 @@ class PhoneTest {
 	@Test
 	void shouldNotCreateInvalidPhoneDDD() {
 	
-		assertThrows(IllegalContactException.class, () -> new Phone(null, "123456789", TypePhone.CELULAR));
-		assertThrows(IllegalContactException.class, () -> new Phone("", "123456789", TypePhone.CELULAR));
-		assertThrows(IllegalContactException.class, () -> new Phone("1", "123456789", TypePhone.CELULAR));
+		assertThrows(IllegalContactException.class, () -> new Phone(null, "123456789", TypePhone.MOBILE));
+		assertThrows(IllegalContactException.class, () -> new Phone("", "123456789", TypePhone.MOBILE));
+		assertThrows(IllegalContactException.class, () -> new Phone("1", "123456789", TypePhone.MOBILE));
 	}
 	
 	@Test
 	void shouldNotCreateInvalidPhone() {
 		
-		assertThrows(IllegalContactException.class, () -> new Phone("11", null, TypePhone.CELULAR));
-		assertThrows(IllegalContactException.class, () -> new Phone("11", "", TypePhone.CELULAR));
-		assertThrows(IllegalContactException.class, () -> new Phone("11", "12345678", TypePhone.CELULAR));
+		assertThrows(IllegalContactException.class, () -> new Phone("11", null, TypePhone.MOBILE));
+		assertThrows(IllegalContactException.class, () -> new Phone("11", "", TypePhone.MOBILE));
+		assertThrows(IllegalContactException.class, () -> new Phone("11", "12345678", TypePhone.MOBILE));
 	}
 	
 	@Test
 	void shoudCreatePhone() {
 		String validDDD = "11";
 		String validPhone = "123456789";
-		TypePhone validTypePhone = TypePhone.CELULAR;
+		TypePhone validTypePhone = TypePhone.MOBILE;
 		
 		Phone phone = new Phone(validDDD, validPhone, validTypePhone);
 		
