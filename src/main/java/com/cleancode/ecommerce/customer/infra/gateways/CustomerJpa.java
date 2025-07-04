e97e8e4cc1d88a51c506bc06c346e96149f779be
softwarekleberson <santossilvakleberson@gmail.com>
2025-07-04 20:08:44 -0300

    feat: adding query inner join betwen customer, charge and delivery

--- src/main/java/com/cleancode/ecommerce/customer/infra/gateways/CustomerJpa.java
+++ src/main/java/com/cleancode/ecommerce/customer/infra/gateways/CustomerJpa.java
@@ -1,11 +1,21 @@
 package com.cleancode.ecommerce.customer.infra.gateways;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.cleancode.ecommerce.customer.infra.persistence.jpa.customer.CustomerEntity;
 
-public interface CustomerJpa extends JpaRepository<CustomerEntity, UUID>{
+public interface CustomerJpa extends JpaRepository<CustomerEntity, UUID> {
 
+	@Query("""
+			    SELECT c FROM CustomerEntity c
+			    LEFT JOIN FETCH c.deliveryEntities
+			    LEFT JOIN FETCH c.chargeEntities
+			    WHERE c.id = :id
+			""")
+	Optional<CustomerEntity> findFullById(@Param("id") UUID id);
 }
