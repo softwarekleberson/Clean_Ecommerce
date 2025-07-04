f88b91d5854de84fe64427a43f5c37fd3251f442
softwarekleberson <santossilvakleberson@gmail.com>
2025-07-03 21:20:15 -0300

    refactor add @Data in DeliveryEntity

--- src/main/java/com/cleancode/ecommerce/customer/infra/persistence/jpa/address/DeliveryEntity.java
+++ src/main/java/com/cleancode/ecommerce/customer/infra/persistence/jpa/address/DeliveryEntity.java
@@ -3,7 +3,9 @@ package com.cleancode.ecommerce.customer.infra.persistence.jpa.address;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "delivery")
 public class DeliveryEntity extends AdrressEntity {
