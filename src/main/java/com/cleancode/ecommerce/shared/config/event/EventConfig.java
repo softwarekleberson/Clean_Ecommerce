679aa698bffadf375309e0a6d6ffbbe49f7f1744
softwarekleberson <santossilvakleberson@gmail.com>
2025-07-18 16:53:51 -0300

    chore: Package name change

--- src/main/java/com/cleancode/ecommerce/shared/config/event/EventConfig.java
+++ src/main/java/com/cleancode/ecommerce/shared/config/event/EventConfig.java
@@ -1,4 +1,4 @@
-package com.cleancode.ecommerce.shared.config.Event;
+package com.cleancode.ecommerce.shared.config.event;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
