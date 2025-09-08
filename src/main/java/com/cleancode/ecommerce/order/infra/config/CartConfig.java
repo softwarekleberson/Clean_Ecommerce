e07c6f705d9cf194f2de64d28fd709a9b6275d19
softwarekleberson <santossilvakleberson@gmail.com>
2025-09-08 16:42:51 -0300

    feat(config-cart) Added been to list cart

--- src/main/java/com/cleancode/ecommerce/order/infra/config/CartConfig.java
+++ src/main/java/com/cleancode/ecommerce/order/infra/config/CartConfig.java
@@ -7,7 +7,15 @@ import com.cleancode.ecommerce.customer.domain.customer.repository.CustomerRepos
 import com.cleancode.ecommerce.order.application.service.ValidateProductHasStock;
 import com.cleancode.ecommerce.order.application.service.ValidateProductHasStockServiceImpl;
 import com.cleancode.ecommerce.order.application.useCase.AddProductToCartImpl;
+import com.cleancode.ecommerce.order.application.useCase.DeleteAllCartImpl;
+import com.cleancode.ecommerce.order.application.useCase.DeleteUniqueProductCartImpl;
+import com.cleancode.ecommerce.order.application.useCase.ListCartImpl;
+import com.cleancode.ecommerce.order.application.useCase.UpdateCartImpl;
 import com.cleancode.ecommerce.order.application.useCase.contract.AddProductToCart;
+import com.cleancode.ecommerce.order.application.useCase.contract.DeleteAllCart;
+import com.cleancode.ecommerce.order.application.useCase.contract.DeleteUniqueProductCart;
+import com.cleancode.ecommerce.order.application.useCase.contract.ListCart;
+import com.cleancode.ecommerce.order.application.useCase.contract.UpdateCart;
 import com.cleancode.ecommerce.order.domain.cart.repository.CartRepository;
 import com.cleancode.ecommerce.product.domain.repository.ProductRepository;
 import com.cleancode.ecommerce.stock.domain.repository.StockRepository;
@@ -16,20 +24,37 @@ import com.cleancode.ecommerce.stock.domain.repository.StockRepository;
 public class CartConfig {
 
 	@Bean
-	public AddProductToCart addProductToCart (
-			
-			CustomerRepository customerRepository,
-			ProductRepository productRepository,
-			StockRepository stockRepository,
-			CartRepository cartRepository,
-			ValidateProductHasStock validateProduct
-			) {
-		
-		return new AddProductToCartImpl(customerRepository, productRepository, stockRepository, cartRepository, validateProduct);
+	public AddProductToCart addProductToCart(
+
+			CustomerRepository customerRepository, ProductRepository productRepository, StockRepository stockRepository,
+			CartRepository cartRepository, ValidateProductHasStock validateProduct) {
+
+		return new AddProductToCartImpl(customerRepository, productRepository, stockRepository, cartRepository,
+				validateProduct);
+	}
+
+	@Bean
+	public ListCart listCart(CartRepository cartRepository) {
+		return new ListCartImpl(cartRepository);
 	}
 	
 	@Bean
-	public ValidateProductHasStock validateProductHasStock () {
+	public UpdateCart updateCart (CartRepository cartRepository) {
+		return new UpdateCartImpl(cartRepository);
+	}
+	
+	@Bean
+	public DeleteAllCart deleteAllCart (CartRepository cartRepository) {
+		return new DeleteAllCartImpl(cartRepository);
+	}
+	
+	@Bean
+	public DeleteUniqueProductCart deleteUniqueProductCart (CartRepository cartRepository) {
+		return new DeleteUniqueProductCartImpl(cartRepository);
+	}
+
+	@Bean
+	public ValidateProductHasStock validateProductHasStock() {
 		return new ValidateProductHasStockServiceImpl();
 	}
 }
