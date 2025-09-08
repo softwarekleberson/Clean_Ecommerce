d4fa4e7316c965fccb55be69b98522851f98e2e9
softwarekleberson <santossilvakleberson@gmail.com>
2025-09-08 16:47:17 -0300

    chore(stock-controller): enable CORS with @CrossOrigin(*)

--- src/main/java/com/cleancode/ecommerce/stock/infra/controller/StockController.java
+++ src/main/java/com/cleancode/ecommerce/stock/infra/controller/StockController.java
@@ -2,6 +2,7 @@ package com.cleancode.ecommerce.stock.infra.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/stock")
+@CrossOrigin(origins = "*")
 public class StockController {
 
 	private final CreateStock createStock;
