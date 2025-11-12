b2a703819d33a675fe15d6f8744cc1738752bbe2
softwarekleberson <santossilvakleberson@gmail.com>
2025-11-11 21:53:01 -0300

    chore (pom) add dependency swagger

--- src/main/java/com/cleancode/ecommerce/EcommerceApplication.java
+++ src/main/java/com/cleancode/ecommerce/EcommerceApplication.java
@@ -3,7 +3,11 @@ package com.cleancode.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "E-commerce", description = "E-commerce with Clean Arc and DDD", version = "1"))
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
